7c748e885aacda343bd62654d1f36fa0c9158392
Anuvrat Joshi <joshianuvrat@gmail.com>
2023-04-29 21:19:10 +0530

    added Car Rental project

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -14,15 +14,21 @@ function Portfolio() {
 
         <img src={profile} alt='profile.jpg' />
         <div className="about-content">
-          <p>As a mechanical engineer with a passion for technology, I have developed a diverse set of skills that would be valuable to your company. My knowledge in various technology allows me to approach problems from a unique perspective and develop innovative solutions that deliver real results.</p>
-          <p>My expertise in mechanical engineering has taught me to think critically and logically, and to approach challenges with a systematic and data-driven mindset. I have honed my skills in project management, problem-solving, and communication, which have enabled me to collaborate effectively with cross-functional teams</p>
-          <p><p>With my knowledge and experience, I am confident that I can bring value to your company. I am a quick learner and eager to take on new challenges, and I am committed to continuous learning and self-improvement.</p></p>
+          <p>As a hardworking and motivated individual, I am adept at problem-solving and programming with a keen
+            focus on producing clean, maintainable code. With a strong background in data structures and algorithms, I excel at designing optimal solutions for complex challenges. My ambition and determination drive me to pursue a career in software engineering with a specific focus on full stack development.</p>
+          {/* <p>My expertise in mechanical engineering has taught me to think critically and logically, and to approach challenges with a systematic and data-driven mindset. I have honed my skills in project management, problem-solving, and communication, which have enabled me to collaborate effectively with cross-functional teams</p> */}
+          <p>With my knowledge and experience, I am confident that I can bring value to your company. I am a quick learner and eager to take on new challenges, and I am committed to continuous learning and self-improvement.</p>
         </div>
       </div>
 
       <div className="services-section">
         <h2>My Projects</h2>
         <div className="services-grid">
+          <div className="service-card">
+            <i className="fas fa-code"></i>
+            <a href="https://car-rental-frontend-pt7r.onrender.com" rel="noreferrer" target="_blank"><h3>Car Rental</h3></a>
+            <p>Our car rental platform allows rental agencies to post available cars for rent, which users can easily book and manage in their cart. Rental agencies can view rented cars and manage their listings, while users can quickly and conveniently rent a car. Our platform provides a user-friendly and efficient solution for all car rental needs.</p>
+          </div>
           <div className="service-card">
             <i className="fas fa-code"></i>
             <a href="https://laundary-frontend-g19.onrender.com" rel="noreferrer" target="_blank"><h3>Laundry Cart</h3></a>
